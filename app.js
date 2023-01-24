777c9ad53b0304c8dce79a78e28f9cc37427e432
lilianaplaton <platonliliana1409@gmail.com>
2023-01-24 13:05:34 +0200

    making the navbar for desktop and mobile version, also sidebar menu, video for home page

---
+++ app.js
@@ -0,0 +1,9 @@
+//sidebar menu_toggle
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "230px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
